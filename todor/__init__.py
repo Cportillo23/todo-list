ff031fa0623a00b5eb0208d14078ac718c7c3eec
cportillo <carlosmportillo23@yahoo.com>
2025-01-27 17:08:07 -0600

    Actualizacion: se agrego acceso a BD SQLite

--- todor/__init__.py
+++ todor/__init__.py
@@ -1,5 +1,9 @@
 # Importando la clase Flask
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+#Creando la extension de la Base de Datos
+db = SQLAlchemy()
 
 # Creando función de control
 def create_app():
@@ -10,8 +14,12 @@ def create_app():
     # Configuración del proyecto
     app.config.from_mapping(
         DEBUG = True,
-        SECRET_KEY = 'dev_esit'
+        SECRET_KEY = 'dev_esit',
+        SQLALCHEMY_DATABASE_URI = "sqlite:///todolist.db"
     )
+
+    #Iniciando conexion
+    db.init_app(app)
 
     # Registrando Blueprint
     from . import todo
@@ -25,5 +33,9 @@ def create_app():
     def index():
         return render_template('index.html')
    
+    # Creacion de tablas a partir de los modelos
+    with app.app_context():
+        db.create_all()
+
     return app     
        
